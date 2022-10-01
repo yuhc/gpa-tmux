0da9dde65363c8a7a030bb7ce3e87ea21f5a1f1a
Hangchen YU <yuhc123@gmail.com>
2022-10-01 02:31:45 -0500

    Add a install script for tmux conf and tpm

---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
+
+is_app_installed() {
+  type "$1" &>/dev/null
+}
+
+REPODIR="$(cd "$(dirname "$0")"; pwd -P)"
+cd "$REPODIR";
+
+if [ ! "$REPODIR" = "$HOME/.tmux" ]; then
+  printf "ERROR: Repository must be cloned as \$HOME/.tmux\n"
+  exit 1
+fi
+
+if ! is_app_installed tmux; then
+  printf "WARNING: \"tmux\" command is not found. \
+Install it first\n"
+  exit 1
+fi
+
+if [ ! -e "$REPODIR/plugins/tpm" ]; then
+  printf "WARNING: Cannot found TPM (Tmux Plugin Manager) \
+ at default location: \$HOME/.tmux/plugins/tpm.\n"
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
+if [ -e "$HOME/.tmux.conf" ]; then
+  printf "Found existing .tmux.conf in your \$HOME directory. Will create a backup at $HOME/.tmux.conf.bak\n"
+fi
+
+cp -f "$HOME/.tmux.conf" "$HOME/.tmux.conf.bak" 2>/dev/null || true
+ln -sf "$REPODIR"/.tmux.conf "$HOME"/.tmux.conf
+cp "$REPODIR"/.tmux.conf.local "$HOME"/.tmux.conf.local
+
+# Install TPM plugins.
+# TPM requires running tmux server, as soon as `tmux start-server` does not work
+# create dump __noop session in detached mode, and kill it when plugins are installed
+printf "Install TPM plugins\n"
+tmux new -d -s __noop >/dev/null 2>&1 || true 
+tmux set-environment -g TMUX_PLUGIN_MANAGER_PATH "~/.tmux/plugins"
+"$REPODIR"/plugins/tpm/bin/install_plugins || true
+tmux kill-session -t __noop >/dev/null 2>&1 || true
+
+printf "OK: Completed\n"
